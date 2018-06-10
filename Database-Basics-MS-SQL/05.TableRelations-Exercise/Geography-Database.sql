ddcdc9052dd8e338cb4c5aa9b6b0ae0c5c823266
StefanIvanovIvanov <stefan.ivn.ivn@gmail.com>
2018-06-11 01:54:43 +0300

    Updating Table Relaton Exercises
    Updating Table Relaton Exercises

--- Database-Basics-MS-SQL/05.TableRelations-Exercise/Geography-Database.sql
+++ Database-Basics-MS-SQL/05.TableRelations-Exercise/Geography-Database.sql
@@ -10,7 +10,6 @@ GO
 
 --Problem 9.*Peaks in Rila
 
-USE Geography
 SELECT m.MountainRange, p.PeakName, p.Elevation FROM Mountains AS m
 JOIN Peaks AS p ON p.MountainId = m.Id
 WHERE m.MountainRange = 'Rila'
